c6cd7800d45a9fec35d3da24a31c19489292b6a8
Wessel Steenkamp <66951387+wesse206@users.noreply.github.com>
2021-03-17 13:58:20 +0200

    Add round to end of line

--- LichessToTornelo/pgntornelo.py
+++ LichessToTornelo/pgntornelo.py
@@ -6,12 +6,24 @@ def downloadpgn(tournlink):
 # Function to download the tournament file. The tournlink param gets feeded from the worker function.
 def convertpgn(tournfile, outfile=None):
     count = len(open('tournament.pgn').readlines(  ))
-    totalcount = count/20/5*2
+    totalcount = count/20/5
     # Count the players to know how much profiles to edit
 
-
-
-
+    with open('tournament.pgn', 'r+') as f:
+        lines = f.readlines()
+        round = 1
+        roundline = -1
+        for i, line in enumerate(lines):
+            if line.startswith('[Date "'):
+                roundline += 1
+                if roundline == totalcount:
+                    round += 1
+                    roundline = 0
+                lines[i] = lines[i].strip() + ' [Round "%s"]\n' % str(round)
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+    # Adds the rounds per total games per round
 
 def worker(sendtodwnld=None, outdir=None):
     downloadpgn(sendtodwnld)
