f6e2460cdd2f67ee0240c715a8aae4e7718b39d2
Wessel Steenkamp <66951387+wesse206@users.noreply.github.com>
2021-03-17 19:44:25 +0200

    Enhanced round couting and tried to add firstname to pgn

--- LichessToTornelo/pgntornelo.py
+++ LichessToTornelo/pgntornelo.py
@@ -5,9 +5,16 @@ def downloadpgn(tournlink):
 
 # Function to download the tournament file. The tournlink param gets feeded from the worker function.
 def convertpgn(tournfile, outfile=None):
-    count = len(open('tournament.pgn').readlines(  ))
-    totalcount = count/20/5
+    count = 0
+    
+    with open('tournament.pgn', 'r+') as c:
+        linec = c.readlines()
+        for i, line in enumerate(linec):
+            if line.startswith('[Event '):
+                count += 1
+    count /= 5
     # Count the players to know how much profiles to edit
+    # TODO enhance the counter by matching the UTC times. 
 
     with open('tournament.pgn', 'r+') as f:
         lines = f.readlines()
@@ -16,7 +23,7 @@ def convertpgn(tournfile, outfile=None):
         for i, line in enumerate(lines):
             if line.startswith('[Date "'):
                 roundline += 1
-                if roundline == totalcount:
+                if roundline == count:
                     round += 1
                     roundline = 0
                 lines[i] = lines[i].strip() + ' [Round "%s"]\n' % str(round)
@@ -24,6 +31,16 @@ def convertpgn(tournfile, outfile=None):
         for line in lines:
             f.write(line)
     # Adds the rounds per total games per round
+
+'''
+    with open('tournament.pgn', 'r+') as w:
+        linew = w.readlines()
+        for i, line in enumerate(linew):
+            if line.startswith('[White ') or line.startswith('[Black '):
+                w.seek(8)
+                w.write('L, ')
+# Trying to add a firstname of each player, but still failing 
+'''
 
 def worker(sendtodwnld=None, outdir=None):
     downloadpgn(sendtodwnld)
