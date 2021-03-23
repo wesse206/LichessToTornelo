d53ceacf5d6591645784d0a4b89074510c5ee320
Wessel Steenkamp <66951387+wesse206@users.noreply.github.com>
2021-03-23 21:11:29 +0200

    Added a proper round counter. May need to be modified if Tornelo requires data in strict order

--- LichessToTornelo/pgntornelo.py
+++ LichessToTornelo/pgntornelo.py
@@ -2,47 +2,32 @@ import requests
 def downloadpgn(tournlink):
     tournfiler = requests.get('https://lichess.org/api/swiss/'+tournlink+'/games', allow_redirects=True)
     open('tournament.pgn', 'wb').write(tournfiler.content)
-
 # Function to download the tournament file. The tournlink param gets feeded from the worker function.
+
 def convertpgn(tournfile, outfile=None):
     count = 0
     
     with open('tournament.pgn', 'r+') as c:
-        linec = c.readlines()
+        lines = c.readlines()
         prevline = 0
         gamesperround = 1
         roundtotal = 0
-        for i, line in enumerate(linec):
+        for i, line in enumerate(lines):
             if line.startswith('[UTCTime'):
                 count += 1
                 if prevline == line:
                     gamesperround += 1
+                    lines[i] = lines[i].strip() + ' [Round "%s"]\n' % str(roundtotal)
                 else:
                     gamesperround = 1
                     roundtotal += 1
+                    lines[i] = lines[i].strip() + ' [Round "%s"]\n' % str(roundtotal)
                 prevline = line
     
         print(roundtotal)
-        count /= 5
+        count /= roundtotal
         print(count)
-    # Count the players to know how much profiles to edit
-    # TODO enhance the counter by matching the UTC times. 
-
-    with open('tournament.pgn', 'r+') as f:
-        lines = f.readlines()
-        round = 1
-        roundline = -1
-        for i, line in enumerate(lines):
-            if line.startswith('[Date "'):
-                roundline += 1
-                if roundline == count:
-                    round += 1
-                    roundline = 0
-                lines[i] = lines[i].strip() + ' [Round "%s"]\n' % str(round)
-        f.seek(0)
-        for line in lines:
-            f.write(line)
-    # Adds the rounds per total games per round
+    # Adds the rounds to the file per time
 
 
     with open('tournament.pgn', 'r+') as w:
@@ -62,7 +47,6 @@ def convertpgn(tournfile, outfile=None):
         for line in lines:
             w.write(line)
 # Adds a 'L, ' in front of every player, so that Tornelo can accept the pgn
-
 
 def worker(sendtodwnld=None, outdir=None):
     downloadpgn(sendtodwnld)
